e8bd98bfd3b0d491ca26bf01f74d61e8599b4d75
Vishal Jumnani <vishalj64@users.noreply.github.com>
2022-09-18 19:52:08 +0530

    change the label of email

--- src/components/Login.js
+++ src/components/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
       <form onSubmit={handleSubmit} className="login-form">
         <h3>Login</h3>
         <div>
-          <label htmlFor="email">Email Id :</label>
+          <label htmlFor="email">Email id :</label>
           <input type="email" name="email" id="email" onChange={handleChange} value={email} />
         </div>
         <div>
